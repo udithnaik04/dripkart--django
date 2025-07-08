cfb4b1d70a35b6f3824252986785f96bd7f02bb7
K.C.Udith kumar naik <udithkc@gmail.com>
2025-07-08 15:39:33 +0530

    first commit

---
+++ store/views.py
@@ -0,0 +1,32 @@
+from django.shortcuts import render,get_object_or_404
+from .models import product
+from category.models import Category
+
+def store(request, category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug != None:
+        categories = get_object_or_404(Category ,slug=category_slug)
+        products = product.objects.filter(category = categories,is_available=True)
+        products_count=products.count()
+    else:
+        products = product.objects.all().filter(is_available = True)
+        products_count = products.count()
+    context = {
+            'products':products,
+            'products_count':products_count,
+        }
+    return render (request, 'store/store.html',context)
+
+
+
+def product_detail(request, category_slug, product_slug ):
+    try :
+        single_product = product.objects.get(category__slug=category_slug, slug=product_slug)
+    except Exception as e :
+        raise e
+    context ={
+        'single_product':single_product,
+    }
+    return render(request, 'store/product_detail.html',context)
